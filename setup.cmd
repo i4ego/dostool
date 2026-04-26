19acc3d4721991ad6fd3702ddcdb2016f0f78cbb
i4ego <me@i4ego.ru>
2026-04-26 20:48:46 +0500

    fix setup.cmd file

--- setup.cmd
+++ setup.cmd
@@ -1,11 +1,10 @@
 python3 -m venv .venv
-source ./.venv/bin/activate
+.venv/Scripts/activate.bat
 
-python3 -m pip install -r req.txt
-python3 -m pip install pyinstaller
+pip3 install -r req.txt
+pip3 install pyinstaller
 
-pyinstaller --onefile --console --clean --noconfirm \
- --distpath ./ --name dostool dostool.py
-rm dostool.spec
-rm -rf build
-./dostool
+pyinstaller --onefile --console --clean --noconfirm --distpath ./ --name dostool dostool.py
+remove dostool.spec
+rmdir build /s /q
+dostool
